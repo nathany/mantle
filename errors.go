db43c1bf44ef7ee9346c55479fbc991c615c326a
Nathan Youngman <git@nathany.com>
2012-08-18 20:12:17 -0600

    log errors with a textual representation
    ran into some interesting issues with hex formatting: http://code.google.com/p/go/issues/detail?id=3974
    
    really need to know the function the error occurred in

--- errors.go
+++ errors.go
@@ -3,10 +3,21 @@ package gl3
 // #include <OpenGL/gl3.h>
 import "C"
 
+import (
+  "log"
+)
+
 type ErrorCode Enum
 
 func GetError() ErrorCode {
   return ErrorCode(C.glGetError())
+}
+
+// "glGetError should always be called in a loop, until it returns GL_NO_ERROR"
+func LogErrors() {
+  for code := GetError(); code != NoError; code = GetError() {
+    log.Printf("gl error %s\n", code)
+  }
 }
 
 const (
@@ -19,3 +30,15 @@ const (
   InvalidFramebufferOperation
   NoError ErrorCode = 0
 )
+
+var errorText = map[ErrorCode]string{
+  InvalidEnum:                 "Invalid Enum",
+  InvalidValue:                "Invalid Value",
+  InvalidOperation:            "Invalid Operation",
+  OutOfMemory:                 "Out of Memory",
+  InvalidFramebufferOperation: "Invalid Framebuffer Operation",
+}
+
+func (e ErrorCode) Error() string {
+  return errorText[e]
+}
