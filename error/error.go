b39ce29ee29f0220ac93cf515c08c29f9c093e1e
Nathan Youngman <git@nathany.com>
2012-08-19 04:17:33 -0600

    better logging output for gl errors

--- error/error.go
+++ error/error.go
@@ -4,20 +4,11 @@
 package error
 
 import (
-  "fmt"
-  "log"
-  "runtime"
   "strings"
 )
 
 // Error (could be multiple)
 type Error map[ErrorCode]bool
-
-// Log the error
-func (e Error) Log(skip int) {
-  func_name, file_line := callerInfo(skip)
-  log.Printf("GL error %s in function %s\n  %s\n", e, func_name, file_line)
-}
 
 // Error interface
 func (e Error) Error() string {
@@ -39,16 +30,6 @@ func GetError() Error {
     return err
   }
   return nil
-}
-
-func callerInfo(skip int) (func_name, file_line string) {
-  if pc, file, line, ok := runtime.Caller(skip + 2); ok == true {
-    if fun := runtime.FuncForPC(pc); fun != nil {
-      func_name = fun.Name()
-    }
-    file_line = fmt.Sprintf("%s:%d", file, line)
-  }
-  return func_name, file_line
 }
 
 var errorText = map[ErrorCode]string{
